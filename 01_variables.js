fed34c5eb7107978c5c7b89ee94efc59dbd9c2ca
Abhishek <shaarmabhi27@gmail.com>
2024-06-19 16:58:53 +0530

    add variables datatypes Code

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14455
+let accountEmail= "asharma@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "111111"
+accountCity = "Delhi"
+console.table([accountEmail , accountPassword,accountCity]);
+console.log(accountId);
+
+// Not to use var   Becouseof issue in block scope and functional scope
---
+++ 02_dataTypes.js
@@ -0,0 +1,24 @@
+"Use Strict" // treat all JS Code as newer version
+// alert(3+3) // We are using Node Js, not browser 
+console.log(3+3); //here we are use in node js      
+
+let name = "Abhi" // String
+let age = 22 // Number
+let isLoggedIn = false // boolen
+
+//Number =  2to power of 53
+//  bigint 
+// String ==> ""
+//Boolean => true or false
+// null=> standalone value
+// Undefined
+// Symbol = unique
+
+
+
+
+
+//Object
+
+console.log(typeof null); // Object
+console.log(typeof undefined);// undifined
