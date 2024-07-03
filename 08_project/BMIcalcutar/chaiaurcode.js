f4863b4c2e7cf4a77046d6f362611e21a76b6f5b
Abhishek <shaarmabhi27@gmail.com>
2024-07-03 19:42:40 +0530

    projects work

---
+++ 08_project/BMIcalcutar/chaiaurcode.js
@@ -0,0 +1,29 @@
+/** @format */
+
+const form = document.querySelector('form');
+// console.log(form);
+// This usecase will give you empty
+// const height = parseInt(document.querySelector('#height').value)
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const height = parseInt(document.querySelector('#height').value);
+	const weight = parseInt(document.querySelector('#weight').value);
+	const results = document.querySelector('#results');
+	const weightGuide = document.querySelector('#weight-guide');
+	if (height === '' || height < 0 || isNaN(height)) {
+		results.innerHTML = `Please give me Valid Heights ${height}`;
+	} else if (weight === '' || weight < 0 || isNaN(weight)) {
+		results.innerHTML = `Please give me Valid weights ${weight}`;
+	} else {
+		const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+		if (bmi < 18.6) {
+			const bmiCategory = 'Under Weight';
+		} else if (bmi >= 18.6 && bmi <= 24.9) {
+			bmiCategory = 'Normal Range';
+		} else {
+			bmiCategory = 'Overweight';
+		}
+		weightGuide.innerHTML = `<p>Your BMI is ${bmi} which is considered ${bmiCategory}.</p>`;
+	}
+});
